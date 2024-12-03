5d3ac4570192e8fec0abc3bbb2f776a4e4becb77
feeldacy <husnafelisacahyani@gmail.com>
2024-12-03 13:33:44 +0700

    bugfix #6 added fix

--- database/migrations/2024_11_01_005246_create_transaksi_detail_table.php
+++ database/migrations/2024_11_01_005246_create_transaksi_detail_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->integer('jumlah');
             $table->integer('subtotal');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
