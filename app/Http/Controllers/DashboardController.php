ddd1693bb4d28a4e469c4c8f4c8dcfec0442b79a
feeldacy <husnafelisacahyani@gmail.com>
2024-12-03 14:56:48 +0700

    bugfix #9

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,11 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Transaksi;
+use App\Models\TransaksiDetail;
+use Illuminate\Support\Facades\DB;
+
 class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $total_transaksi = DB::table('transaksi')->count();
+        $transaksi_count = $total_transaksi;
+
+        $total_terjual = TransaksiDetail::sum('jumlah');
+
+        $omset = TransaksiDetail::sum('subtotal');
+
+        return view('dashboard', compact('transaksi_count', 'total_terjual', 'omset'));
     }
 }
