98b1f46571013e0ef86be2aff6a2eb564aff0dcb
feeldacy <husnafelisacahyani@gmail.com>
2024-12-03 15:00:38 +0700

    bugfix #15

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,5 @@
-
+@extends('layouts')
+@section('content')
 <div class="d-flex flex-column gap-4">
     <div class="card">
         <div class="card-header">SUMMARY</div>
@@ -16,7 +17,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Jumlah Item Terjual</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4><span class="badge text-bg-secondary">{{ number_format($total_terjual, 0, '.', '.') }}</span></h4>
                         </div>
                     </div>
                 </div>
@@ -24,7 +25,7 @@
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">Omzet</h5>
-                            <h4><span class="badge text-bg-secondary"></span></h4>
+                            <h4><span class="badge text-bg-secondary">{{ number_format($omset, 0, '.', '.') }}</span></h4>
                         </div>
                     </div>
                 </div>
@@ -36,10 +37,11 @@
         <div class="card-header">DATA</div>
         <div class="card-body">
             <div class="list-group">
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
-                <a href="{{ route('') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
+                <a href="{{ route('transaksi.create') }}" class="list-group-item list-group-item-action">Tambah Transaksi</a>
+                <a href="{{ route('transaksi.index') }}" class="list-group-item list-group-item-action">Lihat Transaksi</a>
+                <a href="{{ route('transaksidetail.index') }}" class="list-group-item list-group-item-action">Lihat Transaksi Detail</a>
             </div>
         </div>
     </div>
 </div>
+@endsection
