8171ded1a1253806cc0162cd45de2313afc909f2
feeldacy <husnafelisacahyani@gmail.com>
2024-12-03 13:33:05 +0700

    bugfix #5 added fix

--- database/migrations/2024_11_01_005141_create_transaksi_table.php
+++ database/migrations/2024_11_01_005141_create_transaksi_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->integer('bayar');
             $table->integer('kembalian');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
